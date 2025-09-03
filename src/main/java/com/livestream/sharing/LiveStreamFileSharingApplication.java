17ecb7b4bfeb818d9c83a58b0afb79a1c13f4ea7
sorabh solanki <sorabhs2009@gmail.com>
2025-09-03 13:31:15 +0530

    Added initial project setup

---
+++ src/main/java/com/livestream/sharing/LiveStreamFileSharingApplication.java
@@ -0,0 +1,11 @@
+package com.livestream.sharing;
+
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Environment;
+
+public class LiveStreamFileSharingApplication extends Application<LiveStreamFileSharingConfiguration> {
+    @Override
+    public void run(LiveStreamFileSharingConfiguration liveStreamFileSharingConfiguration, Environment env) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/livestream/sharing/LiveStreamFileSharingConfiguration.java
@@ -0,0 +1,6 @@
+package com.livestream.sharing;
+
+import io.dropwizard.core.Configuration;
+
+public class LiveStreamFileSharingConfiguration extends Configuration {
+}
